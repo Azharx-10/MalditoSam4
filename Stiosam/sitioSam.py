12c23b87dfe6699db1c5788dc2be52e8e2e00649
Azharx <emilia.munoz@estudiantes.colegiotgs.cl>
2025-09-23 21:47:17 -0400

    Add files via upload

---
+++ Stiosam/sitioSam.py
@@ -0,0 +1,14 @@
+# Importar streamlit
+import streamlit as st
+
+# Configurar la pagina
+st.set_page_config(
+    page_title="_.Azharx._",
+    page_icon=":video_camera:", # Usar el comando python -m rich.emoji para ver lista de emojis
+    layout="centered",
+)
+
+
+#barra de navegación
+
+pg = st.navigation(["HomePage.py","La_verdadera_historia_de_Little_Sam.py", "La_polémica.py", "Cómo_fue_descubierto.py", "La_pelicula.py", "Otras_noticias.py", "Información_autor.py"])
